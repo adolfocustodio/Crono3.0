96baa4d8b573664fd235461c44e4bbe7b08f18e1
adolfocustodio <adolfocustodio14@gmail.com>
2023-12-18 01:01:41 -0300

    sobre categorias...

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 app_name = 'core'
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home')
+    path('', views.HomeView.as_view(), name='home'),
+    path('categoria/criar/', views.CategoriaCreateView.as_view(), name='categoria_form'),
+    path('categoria/listar/', views.CategoriaListView.as_view(), name='categoria_listar'),
+    path('categoria/editar/<int:pk>/', views.CategoriaUpdateView.as_view(), name='categoria_editar'),
+    path('categoria/excluir/<int:pk>/', views.CategoriaDeleteView.as_view(), name='categoria_excluir')
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.views import generic
 from postagens.models import Postagem
+from core.models import Categoria
+from core.forms import CategoriaForm
+from django.urls import reverse_lazy
 
 
 
@@ -10,3 +13,28 @@ class HomeView(generic.TemplateView):
         context = super().get_context_data(**kwargs)
         context['postagens'] = Postagem.objects.all()
         return context
+
+
+class CategoriaCreateView(generic.CreateView):
+    model = Categoria
+    form_class = CategoriaForm
+    success_url = reverse_lazy("core:categoria_listar")
+    template_name = "categorias/categoria_form.html"
+
+
+class CategoriaListView(generic.ListView):
+    model = Categoria
+    form_class = CategoriaForm
+    template_name = "categorias/categoria_list.html"
+
+
+class CategoriaUpdateView(generic.UpdateView):
+    model = Categoria
+    form_class = CategoriaForm
+    success_url = reverse_lazy("core:categoria_listar")
+    template_name = "categorias/categoria_form.html"
+
+
+class CategoriaDeleteView(generic.DeleteView):
+    model = Categoria
+    success_url = reverse_lazy("core:categoria_listar")
