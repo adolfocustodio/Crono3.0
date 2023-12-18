bcd80a147c25e448839642fccae840b151d26157
adolfocustodio <adolfocustodio14@gmail.com>
2023-12-17 23:18:55 -0300

    nao lembro de vdd

--- crono/urls.py
+++ crono/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from django.contrib import admin
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('core.urls', namespace='core')),
+    path('', include('postagens.urls', namespace='postagens')),
     path('admin/', admin.site.urls)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
